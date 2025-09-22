4102413e0f93e45348cdced952458f7002ce7fa6
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-09-22 19:54:51 +0200

    Exercici 4: Primera versiode manera "manual"

--- repas1/R1_ex4/Program.cs
+++ repas1/R1_ex4/Program.cs
@@ -10,6 +10,24 @@ namespace R1_ex4
     {
         static void Main(string[] args)
         {
+            /*4.	Feu un programa per escriure la cadena CADENA d’aquesta forma
+            El programa demana 		Nom: cadena i mostra el triangle que es veu a continuació
+            */
+            //Versió 1
+            string l1 = "c";
+            string l2 = "a";
+            string l3 = "d";
+            string l4 = "e";
+            string l5 = "n";
+            string l6 = "a";
+
+            Console.WriteLine(l1);
+            Console.WriteLine(l1 + l2);
+            Console.WriteLine(l1 + l2 + l3);
+            Console.WriteLine(l1 + l2 + l3 + l4);
+            Console.WriteLine(l1 + l2 + l3 + l4 + l5);
+            Console.WriteLine(l1 + l2 + l3 + l4 + l5 + l6);
+
         }
     }
 }
