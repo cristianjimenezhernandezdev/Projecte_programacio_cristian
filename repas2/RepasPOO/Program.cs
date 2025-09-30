4b1505ede3216489f23d764f35869785d9020c38
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-09-30 16:34:37 +0200

    v1

--- repas2/RepasPOO/Program.cs
+++ repas2/RepasPOO/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace RepasPOO
 {
+    /*1. Volem crear una aplicació que ens permeti gestionar lligues de diferents esports 
+     * com futbol, hoquei, etc. Cada lliga tindrà equips a dins. Cada equip podrà ser de futbol, 
+     * Bàsquet, tennis, etc, i tindrà un nom i un esport. A més els equips guardaran els jugadors 
+     * que el componen. De cada jugador guardarem el seu nom, el dorsal i l'edat. 
+     * El programa ens ha de mostrar quins equips hi ha a la lliga i de cada equip quina 
+     * és la seva alineació.*/
     internal class Program
     {
         static void Main(string[] args)
         {
+        }
+        public class Lliga
+        {
+            private string NomLliga;
+            private Array[] equip;
         }
     }
 }
