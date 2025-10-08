5c6b892eca7c6b29b7732ec78442d7ccbba87f50
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-10-08 16:26:13 +0200

    programa llest

--- repas2/RepasPOO/Program.cs
+++ repas2/RepasPOO/Program.cs
@@ -77,8 +77,101 @@ namespace RepasPOO
                     }
                 }
             }
-        }
+            /*Creem una classe que és publica de la tercera part, lliga. Definim les parts de la classe, 
+             * el nom, una llista de la classe Equips. Fem els constructors que és el nom de la lliga i emplenem
+             * la llista d'esquips amb la funcio del sistema .Add dins ell metode per afegir equip
+             i al final posem funcio per mostrar el contingut igual que amb el dels equips amb un 
+            foreach per recorrer la llista
+             */
+            public class Lliga
+            {
+                public string Nom;
+                public List<Equip> Equips = new List<Equip>();
 
+                public Lliga(string nom)
+                {
+                    Nom = nom;
+                }
+
+                public void AfegirEquip(Equip e)
+                {
+                    Equips.Add(e);
+                }
+
+                public void MostrarEquips()
+                {
+                    Console.WriteLine("Lliga: " + Nom);
+                    if (Equips.Count == 0)
+                    {
+                        Console.WriteLine("OJO!!! No hi ha equips .");
+                    }
+                    else
+                    {
+                        foreach (var e in Equips)
+                        {
+                            e.Jequip();
+                        }
+                    }
+                }
+            }
+
+            /*A din el program creo cada part, empleno llistes i ho mostro
+             */
+            class Program
+            {
+                static void Main()
+                {
+                    // Crear lliga de futbol
+                    Lliga lligaFutbol = new Lliga("Futbol");
+
+                    Equip equip1 = new Equip("FC Motero Malote", Esport.Futbol);
+                    equip1.AfegirJugador(new Jugador("Oscar", 1, 69));
+                    equip1.AfegirJugador(new Jugador("Julia Malote", 5, 27));
+                    equip1.AfegirJugador(new Jugador("Profe cazabichos", 9, 32));
+
+                    Equip equip2 = new Equip("Atlètic Campalans", Esport.Futbol);
+                    equip2.AfegirJugador(new Jugador("Eric Roca", 10, 25));
+                    equip2.AfegirJugador(new Jugador("Joan Mir", 7, 23));
+
+
+                    lligaFutbol.AfegirEquip(equip1);
+                    lligaFutbol.AfegirEquip(equip2);
+
+                    // Crear lliga de bàsquet
+                    Lliga lligaBasquet = new Lliga("Bàsquet");
+
+                    Equip equip3 = new Equip("CB Looney tunes", Esport.Basquet);
+                    equip3.AfegirJugador(new Jugador("Alex Madrid", 01, 28));
+                    equip3.AfegirJugador(new Jugador("Bugs Bunny", 23, 500));
+
+                    Equip equip4 = new Equip("CB Campalans Stars", Esport.Basquet);
+                    equip4.AfegirJugador(new Jugador("Ruben Crack", 01, 28));
+                    equip4.AfegirJugador(new Jugador("Amigo del Ratóon", 23, 40));
+
+                    lligaBasquet.AfegirEquip(equip3);
+                    lligaBasquet.AfegirEquip(equip4);
+
+                    // Crear lliga de Tennis
+                    Lliga lligaTennis = new Lliga("Tennis");
+
+                    Equip equip5 = new Equip("CT Los del tenis", Esport.Tennis);
+                    equip5.AfegirJugador(new Jugador("El Gafas", 04, 28));
+                    equip5.AfegirJugador(new Jugador("Soldado de invierno", 23, 500));
+
+                    Equip equip6 = new Equip("CT 3 són Multitud", Esport.Tennis);
+                    equip6.AfegirJugador(new Jugador("Numero 1", 01, 1));
+                    equip6.AfegirJugador(new Jugador("Numero 2", 02, 2));
+
+                    lligaTennis.AfegirEquip(equip5);
+                    lligaBasquet.AfegirEquip(equip6);
+                    // Mostrar resultats
+                    lligaFutbol.MostrarEquips();
+                    lligaBasquet.MostrarEquips();
+
+                    Console.WriteLine("Gràcies");                    
+                }
+            }
+        }
     }
 }
 
