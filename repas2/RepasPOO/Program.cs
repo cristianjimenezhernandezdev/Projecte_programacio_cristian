7b53656856d86aa8fbffb493c8065918d81b1522
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-10-08 16:03:10 +0200

    Versio afegit les tres classes

--- repas2/RepasPOO/Program.cs
+++ repas2/RepasPOO/Program.cs
@@ -12,15 +12,73 @@ namespace RepasPOO
      * que el componen. De cada jugador guardarem el seu nom, el dorsal i l'edat. 
      * El programa ens ha de mostrar quins equips hi ha a la lliga i de cada equip quina 
      * és la seva alineació.*/
-    internal class Program
+
+    namespace GestioLligues
     {
-        static void Main(string[] args)
+        // Tipus d'esport
+        public enum Esport { Futbol, Basquet, Hoquei, Tennis }
+
+        // Classe Jugador
+        public class Jugador
         {
+            public string Nom;
+            public int Dorsal;
+            public int Edat;
+
+            public Jugador(string nom, int dorsal, int edat)
+            {
+                Nom = nom;
+                Dorsal = dorsal;
+                Edat = edat;
+            }
+
+            public string VeureJugador()
+            {
+                return "Nom:" + Nom + "; Dorsal: " + Dorsal + "; Edat: " + Edat;
+            }
         }
-        public class Lliga
+
+        // Equip declarem l'atribut Nom, cridem el enum del Esport, fem una Llista de Jugadors amb la classe
+        // Jugador fem el constructor on li pasem el nom del equip que pot ser qualsevol i el de l'esport que és
+        //el del enum. 
+        //Fem servir la funcio de sistema .Add per afegir amb un metode un jugador a la llista. 
+        //Quan creem un jugador, executem el metode i l'afegeix a la llista de Jugadors
+        //Fem un metode per Mostrar l'equip
+        public class Equip
         {
-            private string NomLliga;
-            private Array[] equip;
+            public string Nom;
+            public Esport Esport;
+            public List<Jugador> Jugadors = new List<Jugador>();
+
+            public Equip(string nom, Esport esport)
+            {
+                Nom = nom;
+                Esport = esport;
+            }
+
+            public void AfegirJugador(Jugador j)
+            {
+                Jugadors.Add(j);
+            }
+
+            public void Jequip()
+            {
+                Console.WriteLine("Equip: " + Nom + " Esport: " + Esport);
+                if (Jugadors.Count == 0)
+                {
+                    Console.WriteLine("  L'equip no te jugadors.");
+                }
+                else
+                {
+                    Console.WriteLine("  Jugadors de l'equip:");
+                    foreach (var i in Jugadors)
+                    {
+                        Console.WriteLine("   - " + i.VeureJugador());
+                    }
+                }
+            }
         }
+
     }
 }
+
